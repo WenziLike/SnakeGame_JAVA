27df464136f011179dd9df8f2326dd535790c2cb
WenziLike <SlavaMyraxin@icloud.com>
2021-05-08 07:58:18 +0200

    apple.png and GameField modifiers

--- src/GameField.java
+++ src/GameField.java
@@ -2,16 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.util.Random;
 
-public class GameField extends JPanel implements ActionListener {
+public class GameField extends JPanel implements ActionListener{
     private final int SIZE = 320;
     private final int DOT_SIZE = 16;
     private final int ALL_DOTS = 400;
-    private Image dotted;
-    private Image quince;
-    private int quinceX;
-    private int quinceY;
+    private Image dot;
+    private Image apple;
+    private int appleX;
+    private int appleY;
     private int[] x = new int[ALL_DOTS];
     private int[] y = new int[ALL_DOTS];
     private int dots;
@@ -23,101 +25,139 @@ public class GameField extends JPanel implements ActionListener {
     private boolean inGame = true;
 
 
-    public GameField() {
-        setBackground(Color.BLACK);
+    public GameField(){
+        setBackground(Color.black);
         loadImages();
         initGame();
+        addKeyListener(new FieldKeyListener());
+        setFocusable(true);
+
     }
 
-    public void initGame() {
+    public void initGame(){
         dots = 3;
         for (int i = 0; i < dots; i++) {
-            x[i] = 48 - i * DOT_SIZE;
+            x[i] = 48 - i*DOT_SIZE;
             y[i] = 48;
         }
-        timer = new Timer(250, this);
+        timer = new Timer(250,this);
         timer.start();
-        createEquince();
+        createApple();
     }
 
-    public void createEquince() {
-        quinceX = new Random().nextInt(20) * DOT_SIZE;
-        quinceY = new Random().nextInt(20) * DOT_SIZE;
-
+    public void createApple(){
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
     }
 
-    public void loadImages() {
-        ImageIcon iiq = new ImageIcon("quince.png");
-        quince = iiq.getImage();
-
-        ImageIcon iid = new ImageIcon("dotted.png");
-        dotted = iiq.getImage();
+    public void loadImages(){
+        ImageIcon iia = new ImageIcon("apple.png");
+        apple = iia.getImage();
+        ImageIcon iid = new ImageIcon("dot.png");
+        dot = iid.getImage();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        if (inGame) {
-            g.drawImage(quince, quinceX, quinceY, this);
+        if(inGame){
+            g.drawImage(apple,appleX,appleY,this);
             for (int i = 0; i < dots; i++) {
-                g.drawImage(dotted, x[i], y[i], this);
+                g.drawImage(dot,x[i],y[i],this);
             }
+        } else{
+            String str = "Game Over";
+            //Font f = new Font("Arial",14,Font.BOLD);
+            g.setColor(Color.white);
+            // g.setFont(f);
+            g.drawString(str,125,SIZE/2);
         }
     }
 
-    public void move() {
+    public void move(){
         for (int i = dots; i > 0; i--) {
-            x[i] = x[i - 1];
-            y[i] = y[i - 1];
+            x[i] = x[i-1];
+            y[i] = y[i-1];
         }
-        if (left) {
+        if(left){
             x[0] -= DOT_SIZE;
         }
-        if (right) {
+        if(right){
             x[0] += DOT_SIZE;
-        }
-        if (up) {
+        } if(up){
             y[0] -= DOT_SIZE;
-        }
-        if (down) {
+        } if(down){
             y[0] += DOT_SIZE;
         }
     }
 
-    public void chekQuince() {
-        if (x[0] == quinceX && y[0] == quinceY) {
+    public void checkApple(){
+        if(x[0] == appleX && y[0] == appleY){
             dots++;
-            createEquince();
+            createApple();
         }
     }
 
-    public void checkCollisions() {
-        for (int i = dots; i > 0; i--) {
-            if (i > 4 && x[0] == x[i] && y[0] == y[i]) {
+    public void checkCollisions(){
+        for (int i = dots; i >0 ; i--) {
+            if(i>4 && x[0] == x[i] && y[0] == y[i]){
                 inGame = false;
             }
         }
-        if (x[0] > SIZE) {
+
+        if(x[0]>SIZE){
             inGame = false;
         }
-        if (x[0] < 0) {
+        if(x[0]<0){
             inGame = false;
         }
-        if (y[0] > SIZE) {
+        if(y[0]>SIZE){
             inGame = false;
         }
-        if (x[0] < 0) {
+        if(y[0]<0){
             inGame = false;
         }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (inGame) {
-            chekQuince();
+        if(inGame){
+            checkApple();
             checkCollisions();
             move();
+
         }
         repaint();
     }
+
+    class FieldKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+            int key = e.getKeyCode();
+            if(key == KeyEvent.VK_LEFT && !right){
+                left = true;
+                up = false;
+                down = false;
+            }
+            if(key == KeyEvent.VK_RIGHT && !left){
+                right = true;
+                up = false;
+                down = false;
+            }
+
+            if(key == KeyEvent.VK_UP && !down){
+                right = false;
+                up = true;
+                left = false;
+            }
+            if(key == KeyEvent.VK_DOWN && !up){
+                right = false;
+                down = true;
+                left = false;
+            }
+        }
+    }
+
+
 }
